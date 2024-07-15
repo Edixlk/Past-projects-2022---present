78851e670cbdec92ff88c27b3170af2a1bfb8b59
edixlk <114295187+Edixlk@users.noreply.github.com>
2024-07-15 13:06:02 -0400

    Uploaded sign in form

---
+++ sign in form-V3/main.js
@@ -0,0 +1,8 @@
+  // This will show up in the terminal when you click on the debug menu in the browser. //
+  console.log('Hello there. This page does not have any functionality as yet.');
+
+  document.getElementById("login-btn").onclick = function (){
+   console.log('button was pressed.')
+   window.alert('This is not functional as yet.')
+   window.location('404.html')
+  }
